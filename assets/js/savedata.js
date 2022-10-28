d1c8fe574f3bb107a4d64eca0e8a36b36247f49b
Michael Virga <dr.mvirga@gmail.com>
2022-10-27 19:05:26 -0700

    I think this is a less dumb way actually

--- assets/js/savedata.js
+++ assets/js/savedata.js
@@ -1,16 +1,25 @@
+//TODO: these will be populated at runtime
+var totalClicks;
+var happiness;
+var hunger;
+var water;
+
 //load existing data if any
 function loadGame()
 {
     var saveData = JSON.parse(localStorage.getItem("catclicker"));
     if (saveData == null) saveData = [];
+    totalClicks = saveData.totalClicks;
+    happiness = saveData.happiness;
+    hunger = saveData.hunger;
+    water = saveData.water;
 }
 
 //save current data
 function saveGame()
 {
-    //TODO: populate these values from the game at realtime
-    var totalClicks;
-    var happiness;
-    var hunger;
-    var water;
+    saveData.totalClicks = totalClicks;
+    saveData.happiness = happiness;
+    saveData.hunger = hunger;
+    saveData.water = water;
 }
