7cc8ac41a6225f05b83d3f7640226ce21b0b2965
Michael Virga <dr.mvirga@gmail.com>
2022-10-29 20:18:02 -0700

    graphical update with dynamic colors

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,10 @@
 const quoteInterval = 100;
 const needsInterval = 5000;
 const needsGain = 10;
+const colorGreen = "#a4ff94";
+const colorYellow = "#faff94";
+const colorRed = "#ff9494";
+const colorScale = chroma.scale([colorRed, colorYellow, colorGreen]);
 
 //initializing core data
 var saveData;
@@ -16,8 +20,14 @@ const quoteUrl ="https://api.quotable.io/random";
 //document elements
 const el_totalClicks = document.getElementById("totalClicks");
 const el_happiness = document.getElementById("happiness");
+const el_happinessBar = document.getElementById("happinessBar");
+const el_happinessIcon = document.getElementById("happinessIcon");
 const el_hunger = document.getElementById("hunger");
+const el_hungerBar = document.getElementById("hungerBar");
+const el_hungerIcon = document.getElementById("hungerIcon");
 const el_thirst = document.getElementById("thirst");
+const el_thirstBar = document.getElementById("thirstBar");
+const el_thirstIcon = document.getElementById("thirstIcon");
 const el_cat = document.getElementById("imgCat");
 const el_hitboxCat = document.getElementById("hitboxCat");
 const el_food = document.getElementById("imgFood");
@@ -33,8 +43,14 @@ const el_closeModal = document.getElementById("closeModalButton");
 function updateStats()
 {
     el_totalClicks.innerHTML = totalClicks;
+    el_happinessBar.style.backgroundColor = colorScale(happiness/100).toString();
+    el_happinessIcon.style.color = colorScale(happiness/100).darken(3).toString();
     el_happiness.innerHTML = happiness + "%";
+    el_hungerBar.style.backgroundColor = colorScale(1 - (hunger / 100)).toString();
+    el_hungerIcon.style.color = colorScale(1 - (hunger / 100)).darken(3).toString();
     el_hunger.innerHTML = hunger + "%";
+    el_thirstBar.style.backgroundColor = colorScale(1 - (thirst / 100)).toString();
+    el_thirstIcon.style.color = colorScale(1 - (thirst / 100)).darken(3).toString();
     el_thirst.innerHTML = thirst + "%";
 }
 
